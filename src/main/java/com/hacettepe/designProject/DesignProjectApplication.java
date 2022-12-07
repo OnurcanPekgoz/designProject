37c63c9eda025b5eecca2e25285b654354fbe396
ensarsnmz <ensarsnmz@gmail.com>
2022-12-07 15:01:05 +0300

    Initial commit

---
+++ src/main/java/com/hacettepe/designProject/DesignProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hacettepe.designProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacettepe/designProject/DesignProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacettepe.designProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
